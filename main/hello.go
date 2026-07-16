ae0f5ac064b34ee0e1dd2fccb8663fea61937538
lkorsman <lukekorsman@gmail.com>
2026-07-16 11:15:43 -0500

    Refactored Hello method to accept multiple languages

--- main/hello.go
+++ main/hello.go
@@ -2,15 +2,41 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const (
+	finnish = "Finnish"
+	french = "French"
+	spanish = "Spanish"
+	englishHelloPrefix = "Hello, "
+	finnishHelloPrefix = "Hei, "
+	frenchHelloPrefix = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+)
+
+
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case finnish:
+		prefix = finnishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
